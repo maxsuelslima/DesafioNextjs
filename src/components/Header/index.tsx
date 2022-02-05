c26b88584f350296b10125512911e59b1e12d080
Maxsuel <maxsuel@MacBook-Pro-de-Maxsuel.local>
2022-02-05 12:38:00 -0300

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,14 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import styles from './header.module.scss'
+import Link from 'next/link';
+import styles from './header.module.scss';
 
 export default function Header() {
-    return (
-        <>
-            <Link href="/">
-                <Image src="/Logo.svg" width="240px" height="26px" />
-            </Link>
-        </>
-    )
+  return (
+    <header className={styles.postHeader}>
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="logo" className={styles.logo} />
+        </a>
+      </Link>
+    </header>
+  );
 }
